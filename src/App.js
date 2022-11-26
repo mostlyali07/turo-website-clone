34104e7d7eaf683d9cd1a9f2282e6f225ce2459a
Syed Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2022-11-27 04:40:16 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12 text-center ahref">
+        <a href="/" className="mt-2">Turo has launched in New York! Tap to explore cars.</a>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
