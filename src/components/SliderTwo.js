dee8f311beb942fa0b75d0380f3e2d2df500d9c8
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2022-12-01 23:48:46 +0500

    update

--- src/components/SliderTwo.js
+++ src/components/SliderTwo.js
@@ -1,6 +1,11 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Sec_1 from "../Sec_1.jpg";
+import Sec_2 from "../Sec_2.jpg";
+import Sec_3 from "../Sec_3.jpg";
+import Sec_4 from "../Sec_4.jpg";
+import Sec_5 from "../Sec_5.jpg";
+import Sec_6 from "../Sec_6.jpg";
 import "../App.css";
 
 export default function Slider() {
@@ -8,7 +13,7 @@ export default function Slider() {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
-      items: 5,
+      items: 1,
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -16,7 +21,7 @@ export default function Slider() {
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2,
+      items: 1,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
@@ -24,7 +29,7 @@ export default function Slider() {
     },
   };
   return (
-    <div className="Slider">
+    <div className="Slider my-5">
       <Carousel responsive={responsive}>
         <div>
           <div className="container">
@@ -33,13 +38,145 @@ export default function Slider() {
               <div className="col-md-5">
                 <img src={Sec_1} />
               </div>
-              <div className="col-md-5">
+              <div className="col-md-5 Sec_Sec mt-5">
                 <h3>
                   Find the perfect car{" "}
                   <span className="Color_Purple">
-                    to conquer the great outdoors
+                    to
+                    <br /> conquer the great outdoors
                   </span>
                 </h3>
+                <p className="mb-4">
+                  Go prepared in a rugged 4x4 to take on winter roads with ease,
+                  or a camper van to take you to the trees. Browse cars
+                </p>
+                <button>Browse Car</button>
+              </div>
+              <div className="col-md-1"></div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-1"></div>
+              <div className="col-md-5">
+                <img src={Sec_2} />
+              </div>
+              <div className="col-md-5 Sec_Sec mt-5">
+                <h3>
+                  Find the perfect car
+                  <span className="Color_Purple">
+                    to
+                    <br />
+                    unwind for the weekend
+                  </span>
+                </h3>
+                <p className="mb-4">
+                  From daily drivers to spirited sports cars, ditch the grind
+                  with convenient nearby cars.
+                </p>
+                <button>Browse Car</button>
+              </div>
+              <div className="col-md-1"></div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-1"></div>
+              <div className="col-md-5">
+                <img src={Sec_3} />
+              </div>
+              <div className="col-md-5 Sec_Sec mt-5">
+                <h3>
+                  Find the perfect car{" "}
+                  <span className="Color_Purple">
+                    to
+                    <br /> conquer the great outdoors
+                  </span>
+                </h3>
+                <p className="mb-4">
+                  Go prepared in a rugged 4x4 to take on winter roads with ease,
+                  or a camper van to take you to the trees. Browse cars
+                </p>
+                <button>Browse Car</button>
+              </div>
+              <div className="col-md-1"></div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-1"></div>
+              <div className="col-md-5">
+                <img src={Sec_4} />
+              </div>
+              <div className="col-md-5 Sec_Sec mt-5">
+                <h3>
+                  Find the perfect car{" "}
+                  <span className="Color_Purple">
+                    to
+                    <br /> conquer the great outdoors
+                  </span>
+                </h3>
+                <p className="mb-4">
+                  Go prepared in a rugged 4x4 to take on winter roads with ease,
+                  or a camper van to take you to the trees. Browse cars
+                </p>
+                <button>Browse Car</button>
+              </div>
+              <div className="col-md-1"></div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-1"></div>
+              <div className="col-md-5">
+                <img src={Sec_5} />
+              </div>
+              <div className="col-md-5 Sec_Sec mt-5">
+                <h3>
+                  Find the perfect car{" "}
+                  <span className="Color_Purple">
+                    to
+                    <br /> conquer the great outdoors
+                  </span>
+                </h3>
+                <p className="mb-4">
+                  Go prepared in a rugged 4x4 to take on winter roads with ease,
+                  or a camper van to take you to the trees. Browse cars
+                </p>
+                <button>Browse Car</button>
+              </div>
+              <div className="col-md-1"></div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-1"></div>
+              <div className="col-md-5">
+                <img src={Sec_6} />
+              </div>
+              <div className="col-md-5 Sec_Sec mt-5">
+                <h3>
+                  Find the perfect car{" "}
+                  <span className="Color_Purple">
+                    to
+                    <br /> conquer the great outdoors
+                  </span>
+                </h3>
+                <p className="mb-4">
+                  Go prepared in a rugged 4x4 to take on winter roads with ease,
+                  or a camper van to take you to the trees. Browse cars
+                </p>
+                <button>Browse Car</button>
               </div>
               <div className="col-md-1"></div>
             </div>
