8995ccc2c19bc5586cb00bc9f0622ca4787162c2
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2022-12-06 23:53:25 +0500

    update

--- src/components/Dreams.js
+++ src/components/Dreams.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+import BGD from "../lesssmall.webp"
 const Dreams = () => {
   return (
     <>
@@ -16,6 +17,17 @@ const Dreams = () => {
             <button>Explore Travelogues</button>
           </div>
           <div className="col-md-3"></div>
+        </div>
+        <div className="row">
+            <div className="col-md-12 BGD text-center">
+                <div className="Special p-4">
+                    <h6>Featured travelogue</h6>
+                    <h3>An Olympic experience in Washington</h3>
+                    <p>Discover the epic waterfalls, moody weather, and lush forests of coastal Washington.</p>
+                    <span>Read more</span>
+                </div>
+                <img src= {BGD} className="mt-5"/>
+            </div>
         </div>
       </div>
     </>
