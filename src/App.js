b9e56560791fefb7bb4cbc4338b25cd3c44f23ae
Syed Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2022-12-09 23:54:18 +0500

    update

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-12 text-center ahref pt-2">
-            <a href="/">Turo has launched in New York! Tap to explore cars.</a>
+            <a href="/">Turo has launched in New York! Tap to explore cars</a>
           </div>
         </div>
         <div className="row">
